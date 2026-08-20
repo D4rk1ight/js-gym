846197e6ce7e654d0bb1e55874a5e21692683c71
d4rk1ight <graz9079@gmail.com>
2026-08-20 20:18:48 +0300

    Implement timer functionality with start and stop buttons

--- Date/script.js
+++ Date/script.js
@@ -1,1 +1,23 @@
-const title = document.createElement("h1");
+const title = document.querySelector("h1");
+
+function handleButtonClick() {
+  const userDate = new Date();
+
+  const time = userDate.toString().split(" ")[4];
+  const millisecond = userDate.getMilliseconds().toString().padStart(3, "0");
+
+  title.innerText = `${time}:${millisecond}`;
+}
+
+let timerId;
+
+const startTimer = () => {
+  if (timerId !== undefined) return;
+  handleButtonClick();
+  timerId = setInterval(handleButtonClick, 16.6);
+};
+
+const stopTimer = () => {
+  clearInterval(timerId);
+  timerId = undefined;
+};
