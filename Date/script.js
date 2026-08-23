c4241999005f3ccddb985dbd6d2582b8cfabbb28
d4rk1ight <graz9079@gmail.com>
2026-08-23 19:36:04 +0300

    Refactor timer implementation; replace button click handlers with timer instance methods

--- Date/script.js
+++ Date/script.js
@@ -1,61 +1,12 @@
-const title = document.querySelector("h1");
-
 const timer = document.querySelector(".timer");
-
-const hours = timer.querySelector(".timer__hours");
-const minutes = timer.querySelector(".timer__minutes");
-const seconds = timer.querySelector(".timer__seconds");
-const millisecond = timer.querySelector(".timer__milliseconds");
-
-const timeElement = [hours, minutes, seconds, millisecond];
-
-function handleButtonClick() {
-  const userDate = new Date();
-  // const userTime = userDate.toString().split(" ")[4];
-  const userHours = userDate.getHours();
-  const userMinutes = userDate.getMinutes();
-  const userSeconds = userDate.getSeconds().toString().padStart(2, "0");
-  const userMillisecond = userDate
-    .getMilliseconds()
-    .toString()
-    .padStart(3, "0");
-
-  const timeValues = [userHours, userMinutes, userSeconds, userMillisecond];
-
-  timeElement.forEach((element, index) => {
-    element.innerText = timeValues[index];
-  });
-}
-
-let timerId;
-
-let isRunning = false;
-
-const startTimer = () => {
-  if (isRunning) return;
-  isRunning = true;
-  handleButtonClick();
-  timerId = setInterval(handleButtonClick, 16.6);
-};
-
-const stopTimer = () => {
-  console.log(timerId);
-  timerId = clearInterval(timerId);
-  console.log(timerId);
-  isRunning = false;
-};
-
-// Данные, состояние, действие.
 
 class Timer {
   constructor(rootElement) {
-    // console.log(rootElement);
     this.hours = rootElement.querySelector(".timer__hours");
     this.minutes = rootElement.querySelector(".timer__minutes");
     this.seconds = rootElement.querySelector(".timer__seconds");
     this.millisecond = rootElement.querySelector(".timer__milliseconds");
     this.elements = [this.hours, this.minutes, this.seconds, this.millisecond];
-    // console.log(this.elements);
 
     this.timerId = undefined;
     this.isRunning = false;
@@ -63,7 +14,7 @@ class Timer {
 
   update() {
     const userDate = new Date();
-    const userHours = userDate.getHours();
+    const userHours = userDate.getHours().toString().padStart(2, "0");
     const userMinutes = userDate.getMinutes().toString().padStart(2, "0");
     const userSeconds = userDate.getSeconds().toString().padStart(2, "0");
     const userMillisecond = userDate
@@ -76,6 +27,17 @@ class Timer {
     this.elements.forEach((element, index) => {
       element.innerText = values[index];
     });
+  }
+
+  start() {
+    if (this.isRunning) return;
+    this.isRunning = true;
+    this.timerId = setInterval(() => this.update(), 1);
+  }
+
+  stop() {
+    this.timerId = clearInterval(this.timerId);
+    this.isRunning = false;
   }
 }
 
