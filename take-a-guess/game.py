7e759140adfd92061aa69d2fdd23ac7231f00f34
d4rk1ight <graz9079@gmail.com>
2026-09-12 00:01:51 +0300

    Implement letter guessing game logic and keyboard event handling; add input and keyboard modules

--- take-a-guess/game.py
+++ take-a-guess/game.py
@@ -1,23 +1,40 @@
 import random
+import keyboard
 
 WORDS = ["Шишнила", "каратица", "жучка", "пёс"]
 is_running = True
 
 secret_word = random.choice(WORDS)
 guessed_letters = []
+
+
+def stop_program():
+    global is_running
+    is_running = False
+
+
+keyboard.on_press_key("esc", lambda _: stop_program)
+
+
 while is_running:
-    print(secret_word)
-    hidden_word = "*" * len(secret_word)
+    word_mask = "*" * len(secret_word)
 
-    print("Загаданное слово {0}".format(hidden_word))
-    user_input = input("Итак, ваша буква! ")
-    if user_input in secret_word:
-        print("Есть такая буква!")
-        guessed_letters.append(user_input)
-        # print(guessed_letters)
-    else:
-        print("Нет такой буквы!")
-    new_line = ""
+    print("Загаданное слово {0}".format(word_mask))
+    user_input = input("Итак, ваша буква!\n")
+    guessed_letters.append(user_input)
 
-    for guessed_letters in secret_word:
-        print(guessed_letters, secret_word)
+    answer_line = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            answer_line += letter
+        else:
+            answer_line += "*"
+
+    print(answer_line)
+
+    if answer_line == secret_word:
+        print("Молодец, ты угадал!")
+        is_running = False
+else:
+    print("Игра окончена")
---
+++ take-a-guess/input.py
@@ -0,0 +1,6 @@
+user_input = input("Вводи уже что-нибудь, заебал\n")
+
+print(user_input)
+print(type(user_input))
+print(repr(user_input))
+print(len(user_input))
